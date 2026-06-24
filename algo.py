65ad291307845058a25e8886e1465a8a471e5023
mohdyahyakhan <mohdyahyakhan3022@gmail.com>
2026-06-24 09:22:04 +0530

    Update algo.py

--- algo.py
+++ algo.py
@@ -10,6 +10,7 @@ from flask import Flask
 from threading import Thread
 
 # ===== RENDER KE LIYE DUMMY SERVER =====
+import os
 app = Flask('')
 
 @app.route('/')
@@ -17,7 +18,8 @@ def home():
     return "Bot is alive and running"
 
 def run_server():
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))  # Render ka PORT use karo
+    app.run(host='0.0.0.0', port=port)
 
 Thread(target=run_server).start()
 # =======================================
